4989815a96e8385d80cada612bf0211e29f5ceed
oneDotpy <keenardhito@gmail.com>
2026-03-27 15:01:10 -0400

    Hardcode basePath to /portfolio

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'github' ? '/portfolio' : '',
+  basePath: '/portfolio',
   images: {
     unoptimized: true,
   },

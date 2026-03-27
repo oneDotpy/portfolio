72e6884b48cc946cf8c9b52979127608ae0aa79a
oneDotpy <keenardhito@gmail.com>
2026-03-27 14:55:24 -0400

    Fix basePath env var for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.DEPLOY_TARGET === 'github' ? '/portfolio' : '',
+  basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'github' ? '/portfolio' : '',
   images: {
     unoptimized: true,
   },
